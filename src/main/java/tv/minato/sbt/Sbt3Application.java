be75fc2bd87a6033ee91735c4422624f77ada294
Takayuki MINATO <turbo@minato.tv>
2023-04-05 13:13:47 +0900

    First commit

---
+++ src/main/java/tv/minato/sbt/Sbt3Application.java
@@ -0,0 +1,13 @@
+package tv.minato.sbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbt3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tv/minato/sbt/Sbt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.minato.sbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
